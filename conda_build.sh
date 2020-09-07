814bc2df5f587ffc14e23f979423f588f41312c2
kaihsinwu <kaihsinwu@gmail.com>
2020-09-07 14:42:07 -0400

    update conda_build with channel

--- conda_build.sh
+++ conda_build.sh
@@ -1,6 +1,6 @@
 conda config --set anaconda_upload no
    
 OUTPUT_FN=$(conda-build conda_gen/ --output)
-conda-build conda_gen
+conda-build conda_gen -c kaihsinwu -c pytorch
 
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u kaihsinwu $OUTPUT_FN --force
